fd31d3cb2031686c2fa0759effdec117f9ae8307
Juliana Cortes <cortej19@matc.edu>
2023-11-17 15:04:48 -0500

    making api request to the weather app

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -12,11 +12,23 @@ import humidity_icon from '../Assets/humidity.png';
 
 
 const WeatherApp = ()=> {
+
+    let api_key = "3106b1466cef5a6697a705468307f018";
+
+    const search = () =>{
+        const element = document.getElementsByClassName("cityInput")
+        if(element[0].value==="")
+        {
+            return 0;
+        }
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}London&units=Metric&appid=${api_key}`;
+    }
+
     return (
         <div className="container">
          <div className="top-bar">
           <input type="text" className="cityInput" placeholder='Search'/>
-          <div className="search-icon">
+          <div className="search-icon" onClick={()=>{search()}}>
            <img src={search_icon} alt="search icon"/>
           </div>
           </div>
@@ -24,7 +36,7 @@ const WeatherApp = ()=> {
             <img src={cloud_icon}    alt="icon of cloud"/>
           </div>
           <div className="weather-temp">70°</div>
-          <div className="weather-location">Milwaukee</div>
+          <div className="weather-location">London</div>
           <div className="data-container">
              <div className="element">
                 <img src={humidity_icon}    alt="humidity icon" className="icon"/>
@@ -34,7 +46,7 @@ const WeatherApp = ()=> {
                 </div>
              </div>
               <div className="element">
-                <img src={wind_icon}    alt="" className="icon"/>
+                <img src={wind_icon}    alt="wind icon" className="icon"/>
                 <div className="data">
                     <div className="humidity-percent">18 km/h</div>
                     <div className="text">Wind Speed</div>
