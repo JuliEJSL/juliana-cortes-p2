8002d7016f1a42c6018e340066adfc4c4fbc698b
Juliana Cortes <cortej19@matc.edu>
2023-11-16 21:17:50 -0500

    weather App component

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,9 +1,21 @@
 import React from "react";
-import './WeatherApp.css'
+import './WeatherApp.css';
+
+import search_icon from '../Assets/search.png';
+import clear_icon from '../Assets/clear.png';
+import cloud_icon from '../Assets/cloud.png';
+import drizzle_icon from '../Assets/drizzle.png';
+import rain_icon from '../Assets/rain.png';
+import snow_icon from '../Assets/snow.png';
+import wind_icon from '../Assets/wind.png';
+import humidity_icon from '../Assets/humidity.png';
+
 
 const WeatherApp = ()=> {
     return (
-        <div>
+        <div className="container">
+         <div className="top-bar"></div>
+          <input type="text" className="cityInput" placeholder='Search'/>
         </div>
     )
 }
