c05739accee6df1d13919499fd4e7ece3ee0b4cc
Juliana Cortes <cortej19@matc.edu>
2023-11-24 11:00:21 -0500

    update fetch date from api and display in user interface

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -14,14 +14,28 @@ import humidity_icon from '../Assets/humidity.png';
 const WeatherApp = ()=> {
 
     let api_key = "3106b1466cef5a6697a705468307f018";
-
-    const search = () =>{
-        const element = document.getElementsByClassName("cityInput")
+// search function
+    const search = async() => {
+        const element =document.getElementsByClassName("cityInput")
         if(element[0].value==="")
         {
             return 0;
         }
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}London&units=Metric&appid=${api_key}`;
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+
+        let response = await fetch(url);
+        let data = await response.json();
+// Display data into the user interface
+        const humidity =document.getElementsByClassName("humidity-percent");
+        const wind = document.getElementsByClassName("wind-rate");
+        const temperature = document.getElementsByClassName("weather-temp");
+        const location = document.getElementsByClassName("weather-location");
+
+        humidity[0].innerHTML = data.main.humidity + " %";
+        wind[0].innerHTML = data.wind.speed + "km/h";
+        temperature[0].innerHTML = data.main.temp + "°C";
+        location[0].innerHTML = data.name;
+
     }
 
     return (
@@ -48,13 +62,11 @@ const WeatherApp = ()=> {
               <div className="element">
                 <img src={wind_icon}    alt="wind icon" className="icon"/>
                 <div className="data">
-                    <div className="humidity-percent">18 km/h</div>
+                    <div className="wind-rate">18 km/h</div>
                     <div className="text">Wind Speed</div>
                 </div>
              </div>
           </div>
-
-
         </div>
     )
 }
